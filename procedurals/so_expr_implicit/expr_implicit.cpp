cf1d93197784a2d9c0710edd2b651f9e8899b362
Simon Otter <simon.otter@ilpvfx.com>
2021-12-12 17:35:46 +0100

    Basic expression support.

--- procedurals/so_expr_implicit/expr_implicit.cpp
+++ procedurals/so_expr_implicit/expr_implicit.cpp
@@ -1,5 +1,7 @@
 #include <string_view>
 
+#include "tinyexpr/tinyexpr.h"
+
 #include <ai.h>
 
 namespace ParamNames {
@@ -14,12 +16,25 @@ struct ExprImplicitNodeData {
     AtVector domain_max;
 };
 
+auto tryCompile(AtString const &expr, double &x, double &y, double &z)
+    -> te_expr * {
+    te_variable vars[] = {{"x", &x}, {"y", &y}, {"z", &z}};
+    int error = 0;
+    te_expr *exprObj = te_compile(expr.c_str(), vars, 3, &error);
+
+    if (!exprObj) {
+        AiMsgFatal("Compilation of expression failed. Code %d.", error);
+    }
+
+    return exprObj;
+}
+
 AI_VOLUME_NODE_EXPORT_METHODS(ExprImplicitNodeMtd);
 
 node_parameters {
     AiParameterStr(ParamNames::Expression, "x^2 + y^2 + z^2 - 1");
-    //AiParameterVec(ParamNames::DomainMin, -2, -2, -2);
-    //AiParameterVec(ParamNames::DomainMax, 2, 2, 2);
+    // AiParameterVec(ParamNames::DomainMin, -2, -2, -2);
+    // AiParameterVec(ParamNames::DomainMax, 2, 2, 2);
 }
 
 volume_create {
@@ -32,6 +47,9 @@ volume_create {
     nodeData->expr = AiNodeGetStr(node, ParamNames::Expression);
     nodeData->domain_min = AiNodeGetVec(node, ParamNames::DomainMin);
     nodeData->domain_max = AiNodeGetVec(node, ParamNames::DomainMax);
+
+    double x, y, z;
+    te_free(tryCompile(nodeData->expr, x, y, z));
 
     data->bbox = AtBBox(nodeData->domain_min, nodeData->domain_max);
     data->auto_step_size = 0.01f;
@@ -60,11 +78,18 @@ volume_ray_extents {
 }
 
 volume_sample {
+    ExprImplicitNodeData *nodeData =
+        static_cast<ExprImplicitNodeData *>(data->private_info);
     if (channel != AtString("field"))
         return false;
 
-    value->FLT() = std::max(-AiPerlin3(sg->P * 3.0), AiV3Dot(sg->Po, sg->Po) - 1.0f);
+    double x = sg->P.x, y = sg->P.y, z = sg->P.z;
+    te_expr *exprObj = tryCompile(nodeData->expr, x, y, z);
+
+    value->FLT() = te_eval(exprObj);
     *type = AI_TYPE_FLOAT;
+
+    te_free(exprObj);
 
     return true;
 }
